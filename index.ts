9cbfe6bbe80b89711aa58185cc0e58b78595ee08
RageNeko26 <rneko2006@gmail.com>
2023-06-02 20:04:16 +0700

    Adding ORM Mysql for storing data

--- index.ts
+++ index.ts
@@ -1,10 +1,31 @@
 import express, { Express, Request, Response } from "express"
+import bodyParser from "body-parser"
 
-const bodyParser: any = require('body-parser')
-const passport: any = require('passport')
+const passport: any = require("passport")
+const GoogleStrategy: any = require("passport-google-oidc")
+const { Sequelize, DataTypes } = require("sequelize")
 
 const app: Express = express()
-const port: number = 3000
+const port: number = 3001
+const Orm: any = new Sequelize(
+		"test-passport",
+		"root",
+		"",
+		{
+			host: "localhost",
+			dialect: "mysql"
+		}
+	)
+
+passport.use(new GoogleStrategy({
+	clientID: '60848798350-o0lknfj0410at5u8j3jejsm117ptk6ej.apps.googleusercontent.com',
+	clientSecret: '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',
+	callbackURL: 'http://localhost:3001/auth/google/callback'
+},
+ (issuer: any, profile: any, cb: any) => {
+
+ }
+))
 
 app.get('/', (req: Request, res: Response) => {
     res.json({
